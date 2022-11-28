c377a15fe8943feaa5aaefc77fc75c997681e187
unknown <oussama.fajraoui1@gmail.com>
2022-11-28 17:12:58 +0100

    Loading screen

--- react-native-fashion/App.tsx
+++ react-native-fashion/App.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
-import {Text} from 'react-native'
-import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Onboarding from './src/Authentication/Onboarding';
+import { LoadAssets } from "./src/components"
 
+const fonts = {
+  "SFProText-Bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
+  "SFProText-Semibold": require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
+  "SFProText-Regular": require("./assets/fonts/SF-Pro-Text-Regular.otf"),
+};
+const AuthenticationStack = createStackNavigator();
+const AuthenticationNavigator = () => {
+    return (
+    <AuthenticationStack.Navigator>
+         <AuthenticationStack.Screen name='=Onboarding' component={Onboarding}/>
+    </AuthenticationStack.Navigator>
+    );
+}
 export default function App() {
   return (
-    <NavigationContainer>
-      <Text>Hello world</Text>
-    </NavigationContainer>
+    <LoadAssets {...{fonts}}>
+      <AuthenticationNavigator />
+    </LoadAssets>
   );
 }
---
+++ react-native-fashion/src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+//   interface ComponentNameprops {}
+
+  const ComponentName = () => {
+    return <View style={{ flex: 1, backgroundColor: 'cyan' }}/>
+  }
+
+  export default ComponentName;
